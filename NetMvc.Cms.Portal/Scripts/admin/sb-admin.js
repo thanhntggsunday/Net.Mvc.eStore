beca22d3352de0b185e15fb9a6083d3b107426bf
thanhnt <thanhntggsunday@gmail.com>
2025-07-12 09:37:48 +0700

    Fix bug front-end

--- NetMvc.Cms.Portal/Scripts/admin/sb-admin.js
+++ NetMvc.Cms.Portal/Scripts/admin/sb-admin.js
@@ -17,12 +17,14 @@ $(function() {
 //Loads the correct sidebar on window load,
 //collapses the sidebar on window resize.
 $(function() {
-    $(window).bind("load resize", function() {
-        console.log($(this).width())
-        if ($(this).width() < 768) {
-            $('div.sidebar-collapse').addClass('collapse')
-        } else {
-            $('div.sidebar-collapse').removeClass('collapse')
-        }
-    })
-})
+    $(window).bind("load resize",
+        function() {
+            console.log($(this).width())
+            if ($(this).width() < 768) {
+                $('div.sidebar-collapse').addClass('collapse')
+            } else {
+                $('div.sidebar-collapse').removeClass('collapse')
+            }
+        })
+});
+
