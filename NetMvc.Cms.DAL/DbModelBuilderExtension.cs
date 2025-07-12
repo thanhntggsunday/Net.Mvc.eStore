7647d8528746664e2abeb61b10582ad072355cda
thanhnt <thanhntggsunday@gmail.com>
2025-07-12 10:00:10 +0700

    update identity model

--- NetMvc.Cms.DAL/DbModelBuilderExtension.cs
+++ NetMvc.Cms.DAL/DbModelBuilderExtension.cs
@@ -7,10 +7,22 @@ namespace NetMvc.Cms.DAL
     {
         public static void CreateAspNetModel(this DbModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<IdentityRole>().HasKey<string>(r => r.Id).ToTable("AppRoles");
-            modelBuilder.Entity<IdentityUserRole>().HasKey(i => new { i.UserId, i.RoleId }).ToTable("AppUserRoles");
-            modelBuilder.Entity<IdentityUserLogin>().HasKey(i => i.UserId).ToTable("AppUserLogins");
-            modelBuilder.Entity<IdentityUserClaim>().HasKey(i => i.UserId).ToTable("AppUserClaims");
+            modelBuilder.Entity<IdentityRole>()
+                .HasKey<string>(r => r.Id)
+                .ToTable("AppRoles");
+
+            modelBuilder.Entity<IdentityUserRole>()
+                .HasKey(i => new { i.UserId, i.RoleId })
+                .ToTable("AppUserRoles");
+
+            modelBuilder.Entity<IdentityUserLogin>()
+                .HasKey(i => new { i.LoginProvider, i.ProviderKey, i.UserId }) 
+                .ToTable("AppUserLogins");
+
+            modelBuilder.Entity<IdentityUserClaim>()
+                .HasKey(i => i.Id) 
+                .ToTable("AppUserClaims");
         }
+
     }
 }
